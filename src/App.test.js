387fb678200cd7d375f9b137624b2370374f0e82
acidburn <cc.acidburn@gmail.com>
2022-07-12 23:46:49 +0200

    Updated test from regex to literal string

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,10 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
+  // Renders the component to the virtual DOM
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  // I can access elements from the DOMww by using the screen method
+  const linkElement = screen.getByText("Learn React");
+  // Assertion - they start with expect(argument) and are followed by a matcher that runs tests against expected argument
   expect(linkElement).toBeInTheDocument();
 });
